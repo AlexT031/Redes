cac6fe5d64873f27795e2f189cea2e02c00935ec
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-10-21 10:45:42 -0300

    aaa
    aa

--- PH/parte3/db.php
+++ PH/parte3/db.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 function getDBConnection() {
     $db = new PDO('sqlite:supermercado.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
