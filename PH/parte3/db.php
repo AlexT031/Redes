4a76e40aebe405edea9853644dc3dbc203dc11dd
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-10-21 10:48:22 -0300

    a

--- PH/parte3/db.php
+++ PH/parte3/db.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+header('Content-Type: application/json');
+
 function getDBConnection() {
     $db = new PDO('sqlite:supermercado.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
