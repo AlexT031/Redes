a0e3ce337a3ac00941adc4f92ee8b427e71a075d
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-10-22 13:25:44 -0300

    a

--- PH/parte3/index.php
+++ PH/parte3/index.php
@@ -15,7 +15,11 @@
 
         <button class="button" onclick="openModal()">Agregar</button>
         
+
+
         <h2>Lista de Productos</h2>
+        
+        <input type="text" id="searchInput" placeholder="Buscar productos...">
         <button class="button" onclick="reloadTable()">Recargar Tabla</button>
         <iframe id="productTable" src="read.php" style="width: 100%; height: 400px; border: none;"></iframe>
 
