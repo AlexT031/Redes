1f02db586268c90d96821b2e53211f64607e98f9
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-10-22 02:46:26 -0300

    a

--- PH/parte3/db.php
+++ PH/parte3/db.php
@@ -1,11 +1,16 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-header('Content-Type: application/json');
+$host = 'localhost';   // Host donde está la base de datos
+$dbname = 'u663826865_supermercado';  // Nombre de la base de datos
+$username = 'u663826865_AlexT031';  // Usuario de MySQL
+$password = '_kvc?Vpc.-ff4C&';  // Contraseña de MySQL
 
-function getDBConnection() {
-    $db = new PDO('sqlite:supermercado.db');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $db;
+// Crear conexión
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
+
+echo "Conexión exitosa a la base de datos MySQL";
 ?>
