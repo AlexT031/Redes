3a78b3477904e8d9ef069ddcb80ec3de99d648bc
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-10-22 12:06:48 -0300

    a

--- PH/parte3/create.php
+++ PH/parte3/create.php
@@ -1,10 +1,13 @@
 <?php
+// Incluir la conexión a la base de datos
 include 'db.php';
 
+// Obtener los datos del formulario
 $nombre = $_POST['nombre'];
 $codigo = $_POST['codigo'];
 $id_marca = $_POST['id_marca'];
 
+// Insertar el producto en la base de datos
 $sql = "INSERT INTO productos (nombre, codigo, id_marca) VALUES ('$nombre', '$codigo', '$id_marca')";
 if ($conn->query($sql) === TRUE) {
     echo "<script>alert('Producto agregado exitosamente.'); window.location.href='index.html';</script>";
@@ -12,5 +15,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+// Cerrar la conexión a la base de datos
 $conn->close();
 ?>
