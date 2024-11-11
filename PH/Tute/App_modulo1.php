4468ff1c5aecec5f30f4730db4026da7b506e223
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-11-11 19:24:18 -0300

    Update App_modulo1.php

--- PH/Tute/App_modulo1.php
+++ PH/Tute/App_modulo1.php
@@ -278,7 +278,7 @@ if (!isset($_SESSION['usuario'])) {
         <div id="modal-content">
             <button id="close-button" onclick="closeModal()">✖</button>
             <h2>Respuesta del servidor</h2>
-            <iframe id="modal-message" width="100%" height="500px"></iframe>
+            <div id="modal-message" width="100%" height="500px"></div>
         </div>
     </div>
 
