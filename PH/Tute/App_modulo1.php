827cfde6f02d246fc7005dc645239201820b1e72
AlexT031 <114535987+AlexT031@users.noreply.github.com>
2024-11-11 20:17:08 -0300

    Update App_modulo1.php

--- PH/Tute/App_modulo1.php
+++ PH/Tute/App_modulo1.php
@@ -98,23 +98,27 @@ if (!isset($_SESSION['usuario'])) {
         }
 
         .modal {
-            display: none;
+            display: flex;
+            justify-content: center;
+            align-items: center;
             position: fixed;
-            z-index: 1;
+            z-index: 1000;
             left: 0;
             top: 0;
             width: 100%;
             height: 100%;
             background-color: rgba(0, 0, 0, 0.5);
+            /* Fondo oscuro semitransparente */
         }
 
         .modal-content {
             background-color: white;
-            margin: 10% auto;
             padding: 20px;
-            border-radius: 10px;
+            border-radius: 8px;
             width: 60%;
+            max-width: 800px;
             box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+            text-align: center;
         }
 
         .close {
@@ -122,13 +126,12 @@ if (!isset($_SESSION['usuario'])) {
             float: right;
             font-size: 28px;
             font-weight: bold;
+            cursor: pointer;
         }
 
         .close:hover,
         .close:focus {
             color: black;
-            text-decoration: none;
-            cursor: pointer;
         }
 
         input[type="text"],
@@ -456,7 +459,7 @@ if (!isset($_SESSION['usuario'])) {
                     });
             }
         }
-        
+
         function limpiarFiltros() {
             const inputs = document.querySelectorAll(".filter-input");
             inputs.forEach(input => {
